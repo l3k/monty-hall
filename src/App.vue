f4f32cbf7e7eb2b25e463057fe66ffa3c247d6fb
Lucas Krul <l3k.scorp@gmail.com>
2019-06-04 23:33:13 -0300

    Criado o projeto 'Problema de Monty Hall'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>Problema de Monty Hall</h1>
+
+        <!--  -->
+
+        <div class="form">
+            <div v-if="!started">
+                <label for="doorsAmount">Quantas portas?</label>
+                <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount">
+            </div>
+            <div v-if="!started">
+                <label for="selectedDoor">Qual a porta premiada?</label>
+                <input type="text" id="selectedDoor" size="3" v-model.number="selectedDoor">
+            </div>
+            <button v-if="!started" @click="started = true">Iniciar</button>
+            <button v-if="started" @click="started = false">Reiniciar</button>
+        </div>
+        <div class="doors" v-if="started">
+            <div v-for="i in doorsAmount" :key="i">
+                <door :number="i" :hasGift="i == selectedDoor"></door>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Door from './components/Door.vue';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Door
+        },
+        data() {
+            return {
+                started: false,
+                doorsAmount: 3,
+                selectedDoor: null
+            }
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        box-sizing: border-box;
+        font-family: 'Montserrat', 'sans-serif';
+    }
+
+    body {
+        color: #fff;
+        background: #ff7e5f;
+        /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #feb47b, #ff7e5f);
+        /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #feb47b, #ff7e5f);
+        /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+    }
+
+    #app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    #app h1 {
+        border: 1px solid #000;
+        background-color: 1px solid #0004;
+        padding: 20px;
+        margin-bottom: 60px;
+    }
+
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 40px;
+    }
+
+    .form, .form input, .form button {
+        margin-bottom: 10px;
+        font-size: 2rem;
+    }
+
+    .doors {
+        align-self: stretch;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
 </style>
